ed4f96583ae96f53745db19ba4920660e46f8d8a
Johannes Scherer <johannes.scherer@uni-ulm.de>
2023-01-06 12:21:54 +0100

    update optimization

--- hyperparameter_optimization.py
+++ hyperparameter_optimization.py
@@ -16,35 +16,29 @@ def main(config, s_train, p_train, v_train):
 
     training_device = "cuda"
 
-    model_parameters = config["model_parameters"]
-    optimizer_parameters = config["optimizer_parameters"]
-    training_parameters = config["training_parameters"]
-
+    ################################################################################
+    # model parameters
     model = AZNeuralNetwork(
         game_size=3,
-        model_parameters=model_parameters,
+        model_parameters={"hidden_layers": [1024, 1024, 1024, 512], "dropout": 0.2},
         inference_device=None
     )
 
-    # parameters
-    game_buffer_size = training_parameters["game_buffer_size"]
-    epochs = training_parameters["epochs"]
-    batch_size = training_parameters["batch_size"]
-    patience = training_parameters["patience"]
+    # training parameters
+    epochs = 100000000
+    patience = 100000000
+    batch_size = 2048
 
+    # optimization parameters
     optimizer = torch.optim.SGD(
         model.parameters(),
-        lr=optimizer_parameters["learning_rate"],
-        momentum=optimizer_parameters["momentum"],
-        weight_decay=optimizer_parameters["weight_decay"],
+        lr=0.01,
+        momentum=0.9,
+        weight_decay=0.00001,
     )
+    ################################################################################
 
     print(f"The dataset consist of {len(train_examples)} training examples (including augmentations).")
-
-    # TODO "Parameters were updated from 700,000 mini-batches of 2,048 positions. ...
-    # TODO ... Each mini-batch of data is  sampled uniformly at random from all positions of the most recent 500,000 games (a 200 moves = 10M examples)
-    #  (we: 100,000 games a 18 moves = 1.8M)
-    # TODO of self-play"
 
     s_batched, p_batched, v_batched = [], [], []
     for i in tqdm(range(0, len(train_examples), batch_size), file=stdout):
@@ -52,9 +46,9 @@ def main(config, s_train, p_train, v_train):
         p_batched.append(torch.tensor(np.vstack(p_train[i:i + batch_size]), dtype=torch.float32, device=training_device))
         v_batched.append(torch.tensor(v_train[i:i + batch_size], dtype=torch.float32, device=training_device))  # scalar v
     n_batches = len(s_batched)
+    print(f"# batches: {n_batches}")
 
     current_patience = 0
-    best_model = None
     best_loss = 1e10
 
     CrossEntropyLoss = torch.nn.CrossEntropyLoss()
@@ -72,7 +66,7 @@ def main(config, s_train, p_train, v_train):
 
         # train model
         model.train()
-        for i in range(n_batches):
+        for i in tqdm(range(n_batches)):
             optimizer.zero_grad()
 
             p, v = model.forward(s_batched[i])
@@ -96,27 +90,32 @@ def main(config, s_train, p_train, v_train):
 
             if loss < best_loss:
                 best_loss = loss
-                best_model = deepcopy(model)
                 current_patience = 0
                 print("New best model achieved!")
             else:
                 current_patience += 1
                 print("")
 
-    model = best_model
 
-
-
-
+"""
+- AlphaGo Zero: - 700,000 mini-batches of 2048 training examples (= 1,433,000,000 training examples, around 1.8 epochs then)
+                - sampled from 500,000 games (from 20 iterations a 25,000) (a 200 moves = 100,000,000 training examples -> augmented: 800,000,000 training examples)
+-           we: - 8,000 mini-batches of 2048 training examples (= 16,384,000 training examples, around 2 epochs then)
+                - 16,000 mini-batches of 1024 training examples (= 16,384,000 training examples, around 2 epochs then)
+                - 32,000 mini-batches of 512 training examples (= 16,384,000 training examples, around 2 epochs then)
+                - sampled from 50,000 games (from 20 iterations a 2,500) (a 20 moves = 1,000,000 training examples -> augmented: 8,000,000 training examples)
+- INFO: Only train for around 2 epochs per training loop iteration makes sense: each recorded data will be used in training of 20 iterations, while
+  training data always improves in parallel (-> does not make sense to train longer with old data anyway)
+"""
+# TODO remove patience as it does not make sense anymore
 if __name__ == '__main__':
     CONFIG_FILE = "resources/train_config.yaml"
 
-    with open('data/optimization_data.json', 'r') as f:
+    with open('data/optimization_data_augmented.json', 'r') as f:
         train_examples = json.load(f)
-        print(type(train_examples))
+        train_examples = train_examples[:8000000]
 
     print(f"# train examples: {len(train_examples)}")
-    print("extracting dict ..")
     train_examples = [(t["s"], t["p"], t["v"]) for t in train_examples]
     shuffle(train_examples)
     s_train, p_train, v_train = [list(t) for t in zip(*train_examples)]
